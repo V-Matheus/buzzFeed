da37f9485b9e5e89afd4d21a71e56e63238847cf
Victor Matheus <victormatheus507@gmail.com>
2024-02-24 00:07:08 -0300

    calculado o resultado

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -41,7 +41,7 @@ export class QuizzComponent implements OnInit {
   }
   playerChose(value:string) {
     this.answers.push(value)
-
+    this.nextStep()
   }
 
   async nextStep() {
@@ -50,7 +50,21 @@ export class QuizzComponent implements OnInit {
     if(this.questionMaxIndex > this.questionIndex) {
       this.questionsSelected = this.questions[this.questionIndex]
     } else {
+      const finalAnswer: string = await this.checkResult(this.answers)
       this.finished = true
+      this.answersSelected = quizz_question.results[finalAnswer as keyof typeof quizz_question.results]
     }
   }
+
+  async checkResult(answers: string[]) {
+    const result = answers.reduce((previous, current, i, arr) => {
+      if(arr.filter(item => item === previous).length > arr.filter(item => item === current).length) {
+        return previous
+      } else {
+        return current
+      }
+    })
+    return  result
+  }
+
 }
