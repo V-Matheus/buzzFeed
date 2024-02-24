1f666e8097f846394d5a0ff1d367a053b79dcf27
Victor Matheus <victormatheus507@gmail.com>
2024-02-23 23:42:09 -0300

    propriedades logicas

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quizz',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
 export class QuizzComponent {
+  title: string = ''
+  questions: any
+  questionsSelected: any
+
+  answers:string[] = []
+  answersSelected:string = ''
+
+  questionIndex: number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = false
 
 }
