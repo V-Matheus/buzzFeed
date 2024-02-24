7655c2f99c63899ef5b3414fe1dcc2235c33faaa
Victor Matheus <victormatheus507@gmail.com>
2024-02-23 23:56:11 -0300

    criando as regras

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import quizz_question from '../../../assets/data/quizzQuestion.json'
 
 @Component({
   selector: 'app-quizz',
@@ -8,7 +9,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
   title: string = ''
   questions: any
   questionsSelected: any
@@ -21,4 +22,35 @@ export class QuizzComponent {
 
   finished:boolean = false
 
+  ngOnInit(): void {
+    if(quizz_question) {
+      this.finished = false
+      this.title = quizz_question.title
+
+      this.questions = quizz_question.questions
+      this.questionsSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+
+      console.log(this.questionIndex);
+      console.log(this.questionMaxIndex);
+
+    }
+
+  }
+  playerChose(value:string) {
+    this.answers.push(value)
+
+  }
+
+  async nextStep() {
+    this.questionIndex++
+
+    if(this.questionMaxIndex > this.questionIndex) {
+      this.questionsSelected = this.questions[this.questionIndex]
+    } else {
+      this.finished = true
+    }
+  }
 }
